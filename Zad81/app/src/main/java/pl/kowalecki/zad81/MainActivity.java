2f1e0e19ed9100a813c71b28e122c9402fa9d02a
pkowalecki <kowalecki13pl@o2.pl>
2020-06-22 12:55:12 +0200

    Zad8.1 - Odometer

---
+++ Zad81/app/src/main/java/pl/kowalecki/zad81/MainActivity.java
@@ -0,0 +1,73 @@
+package pl.kowalecki.zad81;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends Activity {
+
+    private OdometerService odometerService;
+    private boolean bound = false;
+
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder binder) {
+            OdometerService.OdometerBinder odometerBinder = (OdometerService.OdometerBinder) binder;
+            odometerService = odometerBinder.getOdometer();
+            bound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            bound = false;
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        watchMileage();
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Intent intent = new Intent(this, OdometerService.class);
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        if(bound){
+            unbindService(connection);
+            bound = false;
+        }
+    }
+
+    private void watchMileage(){
+        final TextView distanceView = (TextView)findViewById(R.id.distance);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                double distance = 0.0;
+                if(odometerService != null){
+                    distance = odometerService.getDistanceInMeters();
+                }
+                String distanceStr = String.format("%1$,.2f kilometra", distance);
+                distanceView.setText(distanceStr);
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+}

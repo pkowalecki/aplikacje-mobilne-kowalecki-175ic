544057ad4a39dde3d5a868bbf236fa69df78b66f
pkowalecki <kowalecki13pl@o2.pl>
2020-07-06 10:47:26 +0200

    Bug fix + jpg

--- Zad81/app/src/main/java/pl/kowalecki/zad81/OdometerService.java
+++ Zad81/app/src/main/java/pl/kowalecki/zad81/OdometerService.java
@@ -1,8 +1,10 @@
 package pl.kowalecki.zad81;
 
+import android.Manifest;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -11,6 +13,7 @@ import android.os.Bundle;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 
 public class OdometerService extends Service {
 
@@ -18,23 +21,24 @@ public class OdometerService extends Service {
     private static double distanceInMeters;
     private static Location lastLocation = null;
 
-    public class OdometerBinder extends Binder{
-        OdometerService getOdometer(){
+    class OdometerBinder extends Binder {
+        OdometerService getOdometer() {
             return OdometerService.this;
         }
     }
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return binder;
     }
 
     @Override
-    public void onCreate(){
+    public void onCreate() {
         LocationListener listener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                if (lastLocation == null){
+                if (lastLocation == null) {
                     lastLocation = location;
                 }
                 distanceInMeters += location.distanceTo(lastLocation);
@@ -57,7 +61,17 @@ public class OdometerService extends Service {
             }
         };
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000,1, listener);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, listener);
     }
     public double getDistanceInMeters(){
         return this.distanceInMeters /1000;
